0aa5ee88ca7c1bb2cb626523e5799a718ec08bf4
mamudogurban <ulgenalpresit@gmail.com>
2018-05-10 17:17:30 +0300

    yeni
    sasas

---
+++ Yeni/Assets/scrpits/control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public GameObject zemin1;
+    public GameObject zemin2;
+    float uzunluk = 0;
+
+    public GameObject player;
+
+
+
+    void Start()
+    {
+        uzunluk = zemin1.GetComponent<BoxCollider>().size.x;
+
+    }
+
+
+    void Update()
+    {
+
+
+
+
+        if (player.transform.position.x/3> zemin1.transform.position.x)
+        {
+            zemin1.transform.position += new Vector3(zemin1.transform.position.x*4.2f ,0);
+        }
+        if (player.transform.position.x/3> zemin2.transform.position.x)
+        {
+            zemin2.transform.position += new Vector3(zemin2.transform.position.x *4.2f, 0);
+        }
+    }
+}
---
+++ Yeni/Assets/scrpits/kontrol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kontrol : MonoBehaviour {
+    public GameObject obj;
+
+    void Start()
+    {
+        Spawn();
+    }
+
+
+    void Spawn()
+    {
+        Instantiate(obj, transform.position, Quaternion.identity);
+
+
+
+
+    }
+}
+
