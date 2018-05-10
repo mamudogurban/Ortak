994c415c375d40a2b72e750e5b44e9b71df66af1
mamudogurban <ulgenalpresit@gmail.com>
2018-05-10 15:25:21 +0300

    ahanda bu
    asasas

---
+++ Assets/gamecontrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamecontrol : MonoBehaviour
+{
+    public GameObject zemin1;
+    public GameObject zemin2;
+    float uzunluk = 0;
+    public float hiz;
+
+
+    void Start()
+    {
+        uzunluk = zemin1.GetComponent<BoxCollider>().size.x;
+
+       
+    }
+
+
+    void Update()
+    {
+
+        
+
+
+        if (zemin1.transform.position.x <= -uzunluk)
+        {
+            zemin1.transform.position += new Vector3(uzunluk * 2, 0);
+        }
+        if (zemin2.transform.position.x <= -uzunluk)
+        {
+            zemin2.transform.position += new Vector3(uzunluk * 2, 0);
+
+
+
+
+        }
+    }
+}
---
+++ Assets/scrpits/zeminhareket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zeminhareket : MonoBehaviour
+{
+
+    public float hiz;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * hiz * Time.deltaTime);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
