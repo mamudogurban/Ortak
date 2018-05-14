0e53c2fcd8d047e7895be68eb7b3153bc24b5ff5
mamudogurban <ulgenalpresit@gmail.com>
2018-05-14 11:58:21 +0300

    zıplama olayı
    ss

--- Yeni/Assets/scrpits/Karakter.cs
+++ Yeni/Assets/scrpits/Karakter.cs
@@ -3,34 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Karakter : MonoBehaviour {
-    public float zıplamaGucu;
-    Rigidbody2D agirlik;
-    Vector3 vec;
-    void Start()
+    public float fall;
+    public float jump;
+    Rigidbody rb;
+
+    void Awake()
     {
-        agirlik = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
     }
 
 
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-            {
-            agirlik.AddForce(Vector3.up * zıplamaGucu);
+    void Update()  {
 
+        if (rb.velocity.y < 0) 
+            { rb.velocity += Vector3.up * Physics.gravity.y * (fall ) * Time.deltaTime; }
+
+        else if (rb.velocity.y > 0 && Input.GetButtonDown("Jump"))
+        {
+
+            rb.velocity += Vector3.up * Physics.gravity.y * (jump ) * Time.deltaTime;
+
+
+        }
+
+
+    }
 
 
             
         }
 
 
-            
-        }
 
 
-
-
-    }
+    
 
 
---
+++ Yeni/Assets/scrpits/jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+    public float jumpPower;
+    bool ziplamaKontrol;
+	void Start () {
+
+        ziplamaKontrol = false;
+	}
+	
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)&& ziplamaKontrol)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpPower;
+
+            ziplamaKontrol = false;
+        }
+          
+
+
+        
+        }
+
+
+     void OnCollisionEnter(Collision collision)
+    {
+
+
+        ziplamaKontrol = true;
+    }
+}
+
